fcf8480a44d99859eab6e302a36bb3355591774b
Joseph Kioko <kiokocodes@gmail.com>
2023-04-20 04:01:50 +0300

    variable declaration with sample interactive page

---
+++ variable.js
@@ -0,0 +1,6 @@
+function showFullName() {
+let firstName = document.getElementById("firstName").value;
+let lastName = document.getElementById("lastName").value;
+let fullName = firstName + " " + lastName;
+alert("Your full name is: " + fullName);
+}
